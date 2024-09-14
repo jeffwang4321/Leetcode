6accc381edec4349312c73ef8e346ae63a1e801d
Jeff Wang <jeffwang4321@gmail.com>
2024-09-14 12:17:30 -0700

    Refactor getMinimumCost function to ensure non-decreasing pixel intensities in each column

---
+++ companies/IBM/getMinimunCost.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def getMinimumCost(pixelIntensity: List[List[int]]) -> int:
+    """
+    Calculates the minimum total increase required to ensure that
+    each COLUMNS's pixel intensities are non-decreasing from top to bottom.
+
+    Args:
+        pixelIntensity (List[List[int]]): 2D list of pixel intensity values.
+
+    Returns:
+        int: The total increase needed to make all columns non-decreasing.
+    """
+
+    ROWS = len(pixelIntensity)
+    COLS = len(pixelIntensity[0])
+
+    total_difference = 0
+    for col in range(COLS):
+        for row in range(1, ROWS):
+            if pixelIntensity[row][col] < pixelIntensity[row - 1][col]:
+                difference = pixelIntensity[row - 1][col] - pixelIntensity[row][col] + 1
+                total_difference += difference
+                pixelIntensity[row][col] += difference
+
+    return total_difference
+
+
+if __name__ == "__main__":
+    pixelIntensity = [[2, 5], [7, 4], [3, 5]]  # -> [[2, 5], [7, 6], [8, 7]]
+    print(getMinimumCost(pixelIntensity))  # 5 + 2 + 2 = 9
+
+    pixelIntensity = [[5, 3, 4], [2, 6, 1], [1, 5, 2]]  # -> [[5, 3, 4], [6, 6, 5], [7, 7, 6]]
+    print(getMinimumCost(pixelIntensity))  # 4 + 6 + 2 + 4 + 4 = 20
